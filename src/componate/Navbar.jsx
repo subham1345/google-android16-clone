19e241c811a959a95f21e0ccb2b5a1e01168bf9e
Nobisuke19 <subham019650@gmail.com>
2025-06-25 09:20:10 +0530

    sidebar

--- src/componate/Navbar.jsx
+++ src/componate/Navbar.jsx
@@ -1,58 +1,74 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import './Navbar.css';
-import { useState } from 'react';
+import React, { useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
+import { useState } from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
+import Sidebar from "./Sidebar";
 
 const Navbar = () => {
-  const [isopen, setIsopen] = useState(false)
+  const [toggleSidebar, setToggleSidebar] = useState(false);
+  useEffect(()=>{
+    console.log(toggleSidebar);
+  })
+    useEffect(() => {
+    if (toggleSidebar) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
 
-  const sidemenu=()=>{
-   setIsopen(!isopen);
-  }
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [toggleSidebar]);
 
   return (
-
-
-    <nav className="navbar">
-      <div className="navbar-logo">
-        <NavLink to="/about">
-          <span className="g-blue">G</span>
-          <span className="o-red">o</span>
-          <span className="o-yellow">o</span>
-          <span className="g-blue">g</span>
-          <span className="l-green">l</span>
-          <span className="e-red">e</span>
-        </NavLink>
-    <button className='btn-menu' onClick={sidemenu}></button>
-      </div>
-      <ul className="navbar-links">
-        <li>
-          <NavLink to="/about" className="nav-link">
-            About
+    <>
+      <nav className="navbar">
+        <div className="navbar-logo">
+          <i>
+            <RxHamburgerMenu
+              size={25}
+              onClick={() => setToggleSidebar(!toggleSidebar)}
+            />
+          </i>
+          <NavLink to="/about">
+            <span className="g-blue">G</span>
+            <span className="o-red">o</span>
+            <span className="o-yellow">o</span>
+            <span className="g-blue">g</span>
+            <span className="l-green">l</span>
+            <span className="e-red">e</span>
           </NavLink>
-        </li>
-        <li>
-          <NavLink to="/products" className="nav-link">
-            Products
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/company-info" className="nav-link">
-            Company Info
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/news" className="nav-link">
-            News
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+        </div>
+        <ul className="navbar-links">
+          <li>
+            <NavLink to="/about" className="nav-link">
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/products" className="nav-link">
+              Products
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/company-info" className="nav-link">
+              Company Info
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/news" className="nav-link">
+              News
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      {toggleSidebar&&(
+        <Sidebar isOpen={toggleSidebar} onClose={() => setToggleSidebar(false)} />
+      )}
+    </>
   );
 };
 
 export default Navbar;
-
-
-
-
---
+++ src/componate/Sidebar.jsx
@@ -0,0 +1,50 @@
+// Sidebar.js
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Sidebar.css";
+
+const Sidebar = ({ isOpen, onClose }) => {
+  return (
+    <>
+      <div className={`sidebar ${isOpen ? "open" : ""}`}>
+       <div className="sidebar-logo">
+          <NavLink to="/about"  onClick={onClose}>
+            <span className="g-blue">G</span>
+            <span className="o-red">o</span>
+            <span className="o-yellow">o</span>
+            <span className="g-blue">g</span>
+            <span className="l-green">l</span>
+            <span className="e-red">e</span>
+          </NavLink>
+       </div>
+
+        <ul>
+          <li>
+            <NavLink to="/about" onClick={onClose}>
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/products" onClick={onClose}>
+              Products
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/company-info" onClick={onClose}>
+              Company Info
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/news" onClick={onClose}>
+              News
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+
+      {isOpen && <div className="backdrop" onClick={onClose} />}
+    </>
+  );
+};
+
+export default Sidebar;
