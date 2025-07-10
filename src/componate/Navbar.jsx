8c335f30ccf60a62fad6fdfe7467bfe454cfea86
subham1345 <143317553+subham1345@users.noreply.github.com>
2025-07-11 00:38:41 +0530

    fixnavbarandlayout

--- src/componate/Navbar.jsx
+++ src/componate/Navbar.jsx
@@ -1,46 +1,67 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./Navbar.css";
-import { useState } from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
 import Sidebar from "./Sidebar";
-import gimg from "../assets/google-logo.png"; // Adjust the path as necessary
+import gimg from "../assets/google-logo.png"; 
 
 const Navbar = () => {
   const [toggleSidebar, setToggleSidebar] = useState(false);
-  useEffect(()=>{
-    console.log(toggleSidebar);
-  })
-    useEffect(() => {
+  const [showNavbar, setShowNavbar] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [hasScrolled, setHasScrolled] = useState(false);
+
+  
+  useEffect(() => {
     if (toggleSidebar) {
       document.body.style.overflow = "hidden";
     } else {
       document.body.style.overflow = "auto";
     }
-
     return () => {
       document.body.style.overflow = "auto";
     };
   }, [toggleSidebar]);
 
+ 
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      if (currentScrollY > lastScrollY && currentScrollY > 100) {
+        setShowNavbar(false);
+      } else {
+        setShowNavbar(true);
+      }
+
+      setHasScrolled(currentScrollY > 0); 
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [lastScrollY]);
+
   return (
     <>
-      <nav className="navbar">
+      <nav
+        className={`navbar ${showNavbar ? "show" : "hide"} ${
+          hasScrolled ? "scrolled" : ""
+        }`}
+      >
         <div className="navbar-logo">
-          <i style={{ cursor: "pointer",padding: "10px" }}>
+          <i style={{ cursor: "pointer", padding: "10px" }}>
             <RxHamburgerMenu
               size={25}
               onClick={() => setToggleSidebar(!toggleSidebar)}
             />
           </i>
           <NavLink to="/about">
-          <img src={gimg} alt="Google" style={{ width: "100px", height: "auto", margin: "0 10px" }}/>
-            {/* <span className="g-blue">G</span>
-            <span className="o-red">o</span>
-            <span className="o-yellow">o</span>
-            <span className="g-blue">g</span>
-            <span className="l-green">l</span>
-            <span className="e-red">e</span> */}
+            <img
+              src={gimg}
+              alt="Google"
+              style={{ width: "100px", height: "auto", margin: "0 10px" }}
+            />
           </NavLink>
         </div>
         <ul className="navbar-links">
@@ -66,8 +87,12 @@ const Navbar = () => {
           </li>
         </ul>
       </nav>
-      {toggleSidebar&&(
-        <Sidebar isOpen={toggleSidebar} onClose={() => setToggleSidebar(false)} />
+
+      {toggleSidebar && (
+        <Sidebar
+          isOpen={toggleSidebar}
+          onClose={() => setToggleSidebar(false)}
+        />
       )}
     </>
   );
